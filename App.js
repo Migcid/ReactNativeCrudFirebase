97a1b955c94cf94a5d1221c8f63dce9754263ce5
Miguel Cid Y <miguelcidyevenes@gmail.com>
2023-07-31 19:21:29 -0400

    creando constantes y funciones para react Navigation

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native'
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
 
+// para que createNativeStackNavigator funcione debo crear una const
+
+const Stack = createNativeStackNavigator()
+
+
+// para tener mas orden en la app crear una funcion aparte llamada myStack para que contenga las multiples pantallas del NavigationContainer
+
+
+function myStack(){
+  return (
+
+  )
+
+}
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+
+    </NavigationContainer>
   );
 }
 
